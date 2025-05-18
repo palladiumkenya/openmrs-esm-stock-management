43ce0faa78c647910ccd3d724a18c771ac1739e0
Dennis Kigen <kigen.work@gmail.com>
2025-05-18 10:52:12 +0300

    (refactor) Remove custom overlay implementation (#290)
    This PR removes the remnants of the custom overlay implementation which has since been replaced with workspaces. Changes include:
    
    - Removing the custom overlay component system (hook, component, styles)
    - Updating all related imports and references to use the workspace system
    - Cleaning up the codebase to remove any remaining references to the overlay system
    
    Other changes include renaming the stock operations dialog component to stock operations modal for consistency.
    Overall, these changes improve consistency with the framework's workspace pattern and removes the custom overlay implementation in favour of the standard workspace system.

--- src/dashboard/home-dashboard.component.tsx
+++ src/dashboard/home-dashboard.component.tsx
@@ -18,15 +18,12 @@ export default function Dashboard() {
   const activeDashboard = dashboards.find((dashboard) => dashboard.name === params?.dashboard) || dashboards[0];
 
   return (
-    <>
-      <Overlay />
-      <div className={styles.homePageWrapper}>
-        <section className={isDesktop(layout) ? styles.dashboardContainer : styles.dashboardContainerTablet}>
-          {isDesktop(layout) && <ExtensionSlot name="stock-sidebar-slot" key={layout} />}
-          <DashboardView title={activeDashboard?.name} dashboardSlot={activeDashboard?.slot} />
-        </section>
-        <WorkspaceContainer overlay contextKey="stock-management" />
-      </div>
-    </>
+    <div className={styles.homePageWrapper}>
+      <section className={isDesktop(layout) ? styles.dashboardContainer : styles.dashboardContainerTablet}>
+        {isDesktop(layout) && <ExtensionSlot name="stock-sidebar-slot" key={layout} />}
+        <DashboardView title={activeDashboard?.name} dashboardSlot={activeDashboard?.slot} />
+      </section>
+      <WorkspaceContainer overlay contextKey="stock-management" />
+    </div>
   );
 }
